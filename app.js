f8e59a5f2ff1225989d14c8ae5d47b12327613de
Kamil <prospero.apps@gmail.com>
2022-10-20 13:17:33 +0200

    Modified app.js and package.json for production

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://kamil75:mC33kTTr28u@cluster0.xlsmh3y.mongodb.net/inventory_management_app?retryWrites=true&w=majority";
+
+const dev_db_url = "mongodb+srv://kamil75:mC33kTTr28u@cluster0.xlsmh3y.mongodb.net/inventory_management_app?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -32,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(Compression());
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
